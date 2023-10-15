ce3888645cfbfdddcb7c971439937d893f4418f0
Yash Harad <98584619+Yasshharad@users.noreply.github.com>
2023-10-15 11:02:23 +0530

    Updated server.js
    scraping images successfully.

--- svelte-api/server.js
+++ svelte-api/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
+const cors = require('cors');
+const https = require('https');
+
 const app = express();
-const port = 3001;
+const corsOptions = {
+  origin: 'http://localhost:8080',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
@@ -10,36 +20,20 @@ app.post('/api/scrape', async (req, res) => {
   const { url } = req.body;
 
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, {
+      httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+    });
     const $ = cheerio.load(response.data);
 
     const imgUrls = $('img').map((_, img) => $(img).attr('src')).get();
+
     res.json({ success: true, imgUrls });
-    
-    // const allowedExtensions = ['.jpg', '.jpeg', '.png'];
-    // const separatedUrls = {
-    //   jpg: [],
-    //   jpeg: [],
-    //   png: [],
-    //   other: [],
-    // };
-
-    // imgUrls.forEach(url => {
-    //   const ext = url.slice(((url.lastIndexOf(".") - 1) >>> 0) + 2);
-    //   if (allowedExtensions.includes('.' + ext.toLowerCase())) {
-    //     separatedUrls[ext.toLowerCase()].push(url);
-    //   } else {
-    //     separatedUrls.other.push(url);
-    //   }
-    // });
-
-    // res.json({ success: true, separatedUrls });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, error: 'Internal Server Error' });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
